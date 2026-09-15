d29ba5f35aa9a5a7888fe1ef5ea67e6e4c8cb2e2
Kevin <kevmah@udel.edu>
2026-09-15 19:22:38 -0400

    interrupt kind of abandoned

--- src/RGBbutton.c
+++ src/RGBbutton.c
@@ -13,10 +13,6 @@ int cur_led = 0;
 uint32_t last_interrupt_time = 0;
 volatile uint32_t ms = 0;
 
-void delay(volatile uint32_t count){
-    while(count--);
-}
-
 void register_enable(void){
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;//led enable
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;//button enable
@@ -32,10 +28,10 @@ void register_enable(void){
     led_port->MODER |= (0x1 << (red_pin * 2));//set red
 }
 
-void Systick_Handler(void) {
+void SysTick_Handler(void) {
     ms++;
 }
-
+/*
 void EXTI15_10_IRQHandler(void) {
     bool raw_butt = (GPIOC->IDR & (1<<butt_pin)) != 0;
     if (raw_butt) {
@@ -67,7 +63,7 @@ void EXTI15_10_IRQHandler(void) {
     }
     EXTI->PR |= (1<<butt_pin);
 }
-
+*/
 
 int main(void){
     register_enable();
@@ -87,31 +83,32 @@ int main(void){
     while(1) {
         bool raw_butt = (GPIOC->IDR & (1<<butt_pin)) != 0;
         if (raw_butt) {
-            if (ms - last_interrupt_time > debounce_delay) {
+            if (ms- last_interrupt_time > debounce_delay) {
                 last_interrupt_time = ms;
                 if (!butt_lock) {
                     butt_lock = true;
-                    cur_led = (cur_led+1)%3;
+                    cur_led = (cur_led + 1)%3;
+                    if (cur_led == 0) {
+                        led_port->ODR |= (1<<red_pin);
+                        led_port->ODR &= ~(1<<green_pin);
+                        led_port->ODR &= ~(1<<blue_pin);
+                    }
                     if (cur_led == 1) {
                         led_port->ODR |= (1<<green_pin);
-                        led_port->ODR &= ~(1<<blue_pin);
                         led_port->ODR &= ~(1<<red_pin);
+                        led_port->ODR &= ~(1<<blue_pin);
                     }
-                    else if (cur_led == 2) {
-                        led_port->ODR &= ~(1<<green_pin);
+                    if (cur_led == 2) {
                         led_port->ODR |= (1<<blue_pin);
                         led_port->ODR &= ~(1<<red_pin);
-                    }
-                    else if (cur_led == 0) {
                         led_port->ODR &= ~(1<<green_pin);
-                        led_port->ODR &= ~(1<<blue_pin);
-                        led_port->ODR |= (1<<red_pin);
                     }
                 }
             }
         }
-        if (!raw_butt){
+        if (!raw_butt) {
             butt_lock = false;
         }
-    return (0);
+    }
+    return(0);
 }
