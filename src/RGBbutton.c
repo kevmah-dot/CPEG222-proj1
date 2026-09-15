871afb7f3d43b3d1dc038fb948acb1cdcfeeaaf1
Kevin <kevmah@udel.edu>
2026-09-15 16:49:42 -0400

    finish

--- src/RGBbutton.c
+++ src/RGBbutton.c
@@ -84,6 +84,34 @@ int main(void){
 
     NVIC_EnableIRQ(EXTI15_10_IRQn);
     SysTick_Config(SystemCoreClock/1000);
-    while(1);
+    while(1) {
+        bool raw_butt = (GPIOC->IDR & (1<<butt_pin)) != 0;
+        if (raw_butt) {
+            if (ms - last_interrupt_time > debounce_delay) {
+                last_interrupt_time = ms;
+                if (!butt_lock) {
+                    butt_lock = true;
+                    cur_led = (cur_led+1)%3;
+                    if (cur_led == 1) {
+                        led_port->ODR |= (1<<green_pin);
+                        led_port->ODR &= ~(1<<blue_pin);
+                        led_port->ODR &= ~(1<<red_pin);
+                    }
+                    else if (cur_led == 2) {
+                        led_port->ODR &= ~(1<<green_pin);
+                        led_port->ODR |= (1<<blue_pin);
+                        led_port->ODR &= ~(1<<red_pin);
+                    }
+                    else if (cur_led == 0) {
+                        led_port->ODR &= ~(1<<green_pin);
+                        led_port->ODR &= ~(1<<blue_pin);
+                        led_port->ODR |= (1<<red_pin);
+                    }
+                }
+            }
+        }
+        if (!raw_butt){
+            butt_lock = false;
+        }
     return (0);
 }
